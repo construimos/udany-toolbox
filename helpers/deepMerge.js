a8d3010fedf16cbf1d0bf648698d0df7e2fd6d19
udany <long.eagle.goodies@gmail.com>
2021-07-25 16:02:40 -0300

    Fixes implementation of deepmerge so it doesn't fail over undefined objects/arrays in the target object

--- helpers/deepMerge.js
+++ helpers/deepMerge.js
@@ -7,6 +7,14 @@
 export default function deepMerge (target, source) {
 	for (const key of Object.keys(source)) {
 		if (source[key] instanceof Object){
+			if (!target[key]) {
+				if (source[key] instanceof Array) {
+					target[key] = [];
+				} else {
+					target[key] = {};
+				}
+			}
+
 			Object.assign(source[key], deepMerge(target[key], source[key]));
 		}
 	}

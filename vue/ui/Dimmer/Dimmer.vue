640b3fe24977841d7f6860d2b126b2fbc4642e3f
udany <long.eagle.goodies@gmail.com>
2021-11-10 23:46:12 -0300

    Dimmer component

---
+++ vue/ui/Dimmer/Dimmer.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="dimmer" :class="{ show }" :style="{ '--opacity': opacity }" />
+</template>
+
+<script>
+	export default {
+		name: 'Dimmer',
+		props: {
+			show: Boolean,
+			opacity: {
+				type: Number,
+				default: 1
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.dimmer {
+		@include fillAbsolute();
+		position: fixed;
+		pointer-events: none !important;
+
+		background: rgba(black, .5);
+		backdrop-filter: blur(3px);
+
+		opacity: 0;
+
+		transition: opacity .4s;
+
+		&.show {
+			pointer-events: auto !important;
+			opacity: var(--opacity);
+		}
+	}
+</style>

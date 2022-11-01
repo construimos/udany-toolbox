d252a70c2de02e929df2f90876fca51bfb0b1d23
udany <long.eagle.goodies@gmail.com>
2022-11-01 14:18:09 -0300

    Exporting local auth lol

--- modules/auth/index.ts
+++ modules/auth/index.ts
@@ -1,12 +1,14 @@
 import { Auth } from './Auth';
 import { GoogleStrategy } from './strategies/GoogleStrategy';
 import { TwitterStrategy } from './strategies/TwitterStrategy';
+import { LocalStrategy } from './strategies/LocalStrategy';
 import { AuthUser } from './AuthUser';
 import { AuthStrategy } from './AuthStrategy';
 
 const strategies = {
 	GoogleStrategy,
-	TwitterStrategy
+	TwitterStrategy,
+	LocalStrategy
 }
 
 export {
